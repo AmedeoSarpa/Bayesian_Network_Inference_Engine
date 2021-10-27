e0f512447bf1c9bd750b2156bdba8d4a19ef6e95
AmedeoSarpa <amedeosarpa@tiscali.it>
2021-10-27 23:55:22 +0200

    add template

--- projectPDS/cmake-build-debug/cmake_install.cmake
+++ projectPDS/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/proto/Desktop/PdS/Progetto/Progetto GIT/progettoPDS/projectPDS
+# Install script for directory: C:/Users/amede/OneDrive/Desktop/Programmazione di Sistema/myBranchBN/progettoPDS/projectPDS
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/proto/Desktop/PdS/Progetto/Progetto GIT/progettoPDS/projectPDS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/amede/OneDrive/Desktop/Programmazione di Sistema/myBranchBN/progettoPDS/projectPDS/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
